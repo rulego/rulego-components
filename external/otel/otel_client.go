ec64df18cca346dd22840df76e8904917f4832d3
rulego-team <rulego@outlook.com>
2025-01-05 21:02:41 +0800

    feat:add otel component

---
+++ external/otel/otel_client.go
@@ -0,0 +1,519 @@
+package otel
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/rulego/rulego/utils/str"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/rulego/rulego/utils/json"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+
+	"github.com/rulego/rulego"
+	"github.com/rulego/rulego/api/types"
+	"github.com/rulego/rulego/components/base"
+	"github.com/rulego/rulego/utils/el"
+	"github.com/rulego/rulego/utils/maps"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
+	"go.opentelemetry.io/otel/metric"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+)
+
+const (
+	COUNTER   = "COUNTER"
+	GAUGE     = "GAUGE"
+	HISTOGRAM = "HISTOGRAM"
+)
+
+// 配置示例
+//
+//	{
+//		"id": "otel1",
+//		"type": "x/otelMetrics",
+//		"configuration": {
+//		  "endpoint": "localhost:4317",
+//		  "metricsExpr": "${msg.metrics}", // 支持单个指标对象或指标对象数组
+//		  "metrics": [{ // 支持配置方式的指标列表
+//		    "metricName": "http_requests",
+//		    "description": "HTTP requests made",
+//		    "unit": "1",
+//		    "opType": "COUNTER",
+//		    "value": "${msg.value}", // 支持表达式
+//		    "labels": "${msg.labels}" // 支持表达式
+//		  }]
+//		}
+//	  }
+//
+// 注册节点
+func init() {
+	_ = rulego.Registry.Register(&OtelNode{})
+}
+
+// MetricConfig 单个指标配置
+type MetricConfig struct {
+	// 指标名称
+	MetricName string `json:"metricName"`
+	// 指标描述
+	Description string `json:"description"`
+	// Unit 指标的单位，用于描述指标值的度量单位。例如，对于计数器（COUNTER），单位可能是 "1"（表示计数）；
+	// 对于时间度量，单位可能是 "s"（秒）、"ms"（毫秒）、"us"（微秒）或 "ns"（纳秒）；
+	// 对于数据量度量，单位可能是 "B"（字节）；
+	// 对于比例度量，单位可能是 "%"（百分比）。
+	// OpenTelemetry 使用基于 SI（国际单位制）的单位系统，但也支持一些常见的非 SI 单位。
+	// 例如，对于 HTTP 请求的计数，单位可以是 "1"；对于 HTTP 响应时间，单位可以是 "s"。
+	Unit string `json:"unit"`
+	// 操作类型：COUNTER, GAUGE, HISTOGRAM
+	OpType string `json:"opType"`
+	// 指标值，支持动态取值，如：${msg.value} 格式为：float64
+	Value string `json:"value"`
+	// 标签，支持动态取值，如：${msg.labels} 格式为：{"key1":"value1","key2":"value2"}
+	Labels string `json:"labels"`
+}
+
+// MetricValue 单个指标配置，已经获取 Value和 Labels 值
+type MetricValue struct {
+	// 指标名称
+	MetricName string `json:"metricName"`
+	// 指标描述
+	Description string `json:"description"`
+	// Unit 指标的单位，用于描述指标值的度量单位。例如，对于计数器（COUNTER），单位可能是 "1"（表示计数）；
+	// 对于时间度量，单位可能是 "s"（秒）、"ms"（毫秒）、"us"（微秒）或 "ns"（纳秒）；
+	// 对于数据量度量，单位可能是 "B"（字节）；
+	// 对于比例度量，单位可能是 "%"（百分比）。
+	// OpenTelemetry 使用基于 SI（国际单位制）的单位系统，但也支持一些常见的非 SI 单位。
+	// 例如，对于 HTTP 请求的计数，单位可以是 "1"；对于 HTTP 响应时间，单位可以是 "s"。
+	Unit string `json:"unit"`
+	// 操作类型：COUNTER, GAUGE, HISTOGRAM
+	OpType string `json:"opType"`
+	// 指标值
+	Value float64 `json:"value"`
+	// 标签
+	Labels map[string]string `json:"labels"`
+}
+
+func (m MetricValue) GetConfig() MetricConfig {
+	return MetricConfig{
+		MetricName:  m.MetricName,
+		Description: m.Description,
+		Unit:        m.Unit,
+		OpType:      m.OpType,
+		Value:       "",
+		Labels:      "",
+	}
+}
+
+// OtelNodeConfiguration 节点配置
+type OtelNodeConfiguration struct {
+	// OTLP后端系统地址，例如: localhost:4318
+	Server string
+	// 传输协议，支持 grpc 和 http，默认http
+	Protocol string
+	// 动态指标配置和取值表达式，支持单个指标对象或指标对象数组
+	// 如果指标器不存在则，动态创建
+	// MetricExpr 和 Metrics 允许同时存在，合并后发送
+	// 通过expr表达式从消息负荷中获取指标配置
+	// 指标配置示例：
+	// {
+	//  "metrics":[
+	//   {
+	//     "metricName": "http_requests",
+	//     "description": "HTTP requests made",
+	//     "unit": "1",
+	//     "opType": "COUNTER",
+	//     "value": 10,
+	//     "labels": {
+	//       "method": "GET",
+	//       "path": "/api/v1/data"
+	//     }
+	//   }
+	//]
+	//}
+	// 则可以通过 ${msg.metrics} 方式获取指标配置
+	MetricExpr string
+	// 指标配置列表，会在初始化创建指标器
+	// MetricExpr 和 Metrics 允许同时存在，合并后发送
+	Metrics []MetricConfig
+}
+
+// Metric 指标实例
+type Metric struct {
+	Config         MetricConfig
+	ValueTemplate  el.Template
+	LabelsTemplate el.Template
+	Counter        metric.Float64Counter
+	Gauge          metric.Float64UpDownCounter
+	Histogram      metric.Float64Histogram
+}
+
+// OtelNode OpenTelemetry客户端组件，用于记录各种指标
+// 运行通过消息负荷获取指标数据，通过OTLP协议发送到后端系统，如：Prometheus、Datadog、InfluxDB等
+type OtelNode struct {
+	base.SharedNode[*Client]
+	// 节点配置
+	Config OtelNodeConfiguration
+	// 指标缓存,key为指标名称
+	metricsCache map[string]*Metric
+	// 指标缓存锁
+	metricsCacheMu sync.RWMutex
+	// 指标表达式
+	metricsExpr el.Template
+	// OTEL客户端
+	client *Client
+}
+
+// Type 返回组件类型
+func (x *OtelNode) Type() string {
+	return "x/otel"
+}
+
+func (x *OtelNode) New() types.Node {
+	return &OtelNode{
+		Config: OtelNodeConfiguration{
+			Server:     "localhost:4318",
+			Protocol:   "HTTP",
+			MetricExpr: "${msg.metrics}",
+		},
+		metricsCache: make(map[string]*Metric),
+	}
+}
+
+// Init 初始化组件
+func (x *OtelNode) Init(ruleConfig types.Config, configuration types.Configuration) error {
+	err := maps.Map2Struct(configuration, &x.Config)
+	if err != nil {
+		return err
+	}
+	if x.Config.Server == "" {
+		return errors.New("server is required")
+	}
+	// 编译指标表达式
+	if x.Config.MetricExpr != "" {
+		if metricsExpr, err := el.NewTemplate(strings.TrimSpace(x.Config.MetricExpr)); err != nil {
+			return err
+		} else {
+			x.metricsExpr = metricsExpr
+		}
+	}
+	x.Config.Protocol = strings.TrimSpace(strings.ToUpper(x.Config.Protocol))
+	if x.Config.Protocol == "" {
+		x.Config.Protocol = "HTTP"
+	}
+	// 初始化共享MeterProvider
+	err = x.SharedNode.Init(ruleConfig, x.Type(), x.Config.Server, true, func() (*Client, error) {
+		return x.initMeterProvider()
+	})
+
+	// 预编译配置的指标模板
+	for _, metricConfig := range x.Config.Metrics {
+		metricConfig.MetricName = strings.TrimSpace(metricConfig.MetricName)
+		if metricConfig.MetricName == "" {
+			continue
+		}
+		metricConfig.Unit = strings.TrimSpace(metricConfig.Unit)
+		metricConfig.OpType = strings.TrimSpace(metricConfig.OpType)
+		metricConfig.Value = strings.TrimSpace(metricConfig.Value)
+		metricConfig.Labels = strings.TrimSpace(metricConfig.Labels)
+
+		var valueTemplate, labelsTemplate el.Template
+		if metricConfig.Value != "" {
+			valueTemplate, err = el.NewTemplate(metricConfig.Value)
+			if err != nil {
+				return err
+			}
+		}
+		if metricConfig.Labels != "" {
+			labelsTemplate, err = el.NewTemplate(metricConfig.Labels)
+			if err != nil {
+				return err
+			}
+		}
+		m, err := x.getOrCreateMetric(metricConfig)
+		if err != nil {
+			return err
+		}
+		m.ValueTemplate = valueTemplate
+		m.LabelsTemplate = labelsTemplate
+		x.metricsCache[metricConfig.MetricName] = m
+	}
+
+	return nil
+}
+
+type Client struct {
+	MeterProvider *sdkmetric.MeterProvider
+	Meter         metric.Meter
+}
+
+// initMeterProvider 初始化MeterProvider
+func (x *OtelNode) initMeterProvider() (*Client, error) {
+	if x.client != nil && x.client.Meter != nil {
+		return x.client, nil
+	}
+	x.Locker.Lock()
+	defer x.Locker.Unlock()
+	if x.client != nil && x.client.Meter != nil {
+		return x.client, nil
+	}
+	var exporter sdkmetric.Exporter
+	var err error
+
+	// 创建OTLP导出器
+	if strings.ToUpper(x.Config.Protocol) == "GRPC" {
+		// 创建OTLP gRPC导出器
+		exporter, err = otlpmetricgrpc.New(
+			context.Background(),
+			otlpmetricgrpc.WithEndpoint(x.Config.Server),
+			otlpmetricgrpc.WithInsecure(),
+		)
+	} else {
+		// 创建OTLP HTTP导出器
+		exporter, err = otlpmetrichttp.New(
+			context.Background(),
+			otlpmetrichttp.WithEndpoint(x.Config.Server),
+			otlpmetrichttp.WithInsecure(),
+			otlpmetrichttp.WithCompression(otlpmetrichttp.GzipCompression),
+		)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to create OTLP exporter: %v", err)
+	}
+
+	// 创建MeterProvider
+	mp := sdkmetric.NewMeterProvider(
+		sdkmetric.WithReader(
+			sdkmetric.NewPeriodicReader(
+				exporter,
+				sdkmetric.WithInterval(10*time.Second),
+			),
+		),
+	)
+	x.client = &Client{
+		MeterProvider: mp,
+		Meter:         mp.Meter(x.Type()),
+	}
+
+	return x.client, nil
+}
+
+// OnMsg 处理消息
+func (x *OtelNode) OnMsg(ctx types.RuleContext, msg types.RuleMsg) {
+	evn := base.NodeUtils.GetEvnAndMetadata(ctx, msg)
+	var metrics []MetricValue
+	// 处理配置的指标
+	for _, metricConfig := range x.Config.Metrics {
+		m, err := x.getOrCreateMetric(metricConfig)
+		if err != nil {
+			ctx.TellFailure(msg, err)
+			return
+		}
+
+		// 执行值表达式
+		value, err := x.getValue(m.Config, m.ValueTemplate, evn)
+		if err != nil {
+			ctx.TellFailure(msg, err)
+			return
+		}
+
+		// 执行标签表达式
+		attrs, err := x.getLabels(m.Config, m.LabelsTemplate, evn)
+		if err != nil {
+			ctx.TellFailure(msg, err)
+			return
+		}
+		metrics = append(metrics, MetricValue{
+			MetricName:  metricConfig.MetricName,
+			Description: metricConfig.Description,
+			Unit:        metricConfig.Unit,
+			OpType:      metricConfig.OpType,
+			Value:       value,
+			Labels:      attrs,
+		})
+	}
+
+	// 处理动态指标
+	if x.metricsExpr != nil {
+		var out interface{}
+		out, err := x.metricsExpr.Execute(evn)
+		if err != nil {
+			ctx.TellFailure(msg, err)
+			return
+		}
+
+		switch v := out.(type) {
+		case map[string]interface{}:
+			var metricValue MetricValue
+			if err := maps.Map2Struct(v, &metricValue); err != nil {
+				ctx.TellFailure(msg, err)
+				return
+			}
+			metrics = append(metrics, metricValue)
+		case []interface{}:
+			for _, item := range v {
+				if m, ok := item.(map[string]interface{}); ok {
+					var metricValues MetricValue
+					if err := maps.Map2Struct(m, &metricValues); err != nil {
+						ctx.TellFailure(msg, err)
+						return
+					}
+					metrics = append(metrics, metricValues)
+				}
+			}
+		default:
+		}
+
+	}
+	// 处理每个指标
+	for _, cfg := range metrics {
+		if cfg.MetricName == "" {
+			continue
+		}
+		// 获取或创建指标
+		m, err := x.getOrCreateMetric(cfg.GetConfig())
+		if err != nil {
+			ctx.TellFailure(msg, err)
+			return
+		}
+		value := cfg.Value
+		labels := cfg.Labels
+
+		// 转换标签
+		var attrs []attribute.KeyValue
+		for k, v := range labels {
+			attrs = append(attrs, attribute.String(k, v))
+		}
+
+		// 记录指标
+		switch cfg.OpType {
+		case COUNTER:
+			m.Counter.Add(ctx.GetContext(), value, metric.WithAttributes(attrs...))
+		case GAUGE:
+			m.Gauge.Add(ctx.GetContext(), value, metric.WithAttributes(attrs...))
+		case HISTOGRAM:
+			m.Histogram.Record(ctx.GetContext(), value, metric.WithAttributes(attrs...))
+		}
+	}
+	ctx.TellSuccess(msg)
+}
+
+func (x *OtelNode) Destroy() {
+	if x.client != nil && x.client.MeterProvider != nil {
+		_ = x.client.MeterProvider.Shutdown(context.Background())
+		x.client = nil
+	}
+}
+
+// getOrCreateMetric 获取或创建指标
+func (x *OtelNode) getOrCreateMetric(cfg MetricConfig) (*Metric, error) {
+	// 先尝试读锁获取
+	x.metricsCacheMu.RLock()
+	if m, ok := x.metricsCache[cfg.MetricName]; ok {
+		x.metricsCacheMu.RUnlock()
+		return m, nil
+	}
+	x.metricsCacheMu.RUnlock()
+
+	// 获取写锁创建
+	x.metricsCacheMu.Lock()
+	defer x.metricsCacheMu.Unlock()
+
+	// 双重检查,避免重复创建
+	if m, ok := x.metricsCache[cfg.MetricName]; ok {
+		return m, nil
+	}
+	var err error
+	client, err := x.SharedNode.Get()
+	if err != nil {
+		return nil, err
+	}
+	if client.Meter == nil {
+		return nil, errors.New("meter is nil")
+	}
+	m := &Metric{
+		Config: cfg,
+	}
+
+	// 创建指标
+	switch cfg.OpType {
+	case COUNTER:
+		m.Counter, err = client.Meter.Float64Counter(
+			cfg.MetricName,
+			metric.WithDescription(cfg.Description),
+			metric.WithUnit(cfg.Unit),
+		)
+	case GAUGE:
+		m.Gauge, err = client.Meter.Float64UpDownCounter(
+			cfg.MetricName,
+			metric.WithDescription(cfg.Description),
+			metric.WithUnit(cfg.Unit),
+		)
+	case HISTOGRAM:
+		m.Histogram, err = client.Meter.Float64Histogram(
+			cfg.MetricName,
+			metric.WithDescription(cfg.Description),
+			metric.WithUnit(cfg.Unit),
+		)
+	default:
+		return nil, fmt.Errorf("unsupported operation type: %s", cfg.OpType)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	x.metricsCache[cfg.MetricName] = m
+	return m, nil
+}
+
+// getValue 获取指标值
+func (x *OtelNode) getValue(config MetricConfig, valueTemplate el.Template, evn map[string]interface{}) (float64, error) {
+	if valueTemplate == nil {
+		if config.Value != "" {
+			//字符串换换为浮点数
+			if value, err := strconv.ParseFloat(config.Value, 64); err != nil {
+				return 0, err
+			} else {
+				return value, nil
+			}
+		} else {
+			return 0, nil
+		}
+	} else if out, err := valueTemplate.Execute(evn); err != nil {
+		return 0, err
+	} else {
+		switch v := out.(type) {
+		case float64:
+			return v, nil
+		case int:
+			return float64(v), nil
+		case int64:
+			return float64(v), nil
+		case string:
+			return strconv.ParseFloat(config.Value, 64)
+		default:
+			return 0, fmt.Errorf("invalid value type: %T", out)
+		}
+	}
+}
+
+// getLabels 获取标签
+func (x *OtelNode) getLabels(config MetricConfig, labelsTemplate el.Template, evn map[string]interface{}) (map[string]string, error) {
+	if labelsTemplate == nil {
+		if config.Labels != "" {
+			var labels map[string]string
+			if err := json.Unmarshal([]byte(config.Labels), &labels); err == nil {
+				return labels, nil
+			}
+		}
+		return nil, nil
+	} else if out, err := labelsTemplate.Execute(evn); err != nil {
+		return nil, err
+	} else {
+		return str.ToStringMapString(out), nil
+	}
+}
--- go.mod
+++ go.mod
@@ -1,31 +1,41 @@
 module github.com/rulego/rulego-components
 
-go 1.20
+go 1.22.7
 
 require (
 	github.com/IBM/sarama v1.43.2
-	github.com/expr-lang/expr v1.16.0
+	github.com/expr-lang/expr v1.16.9
 	github.com/fullstorydev/grpcurl v1.9.1
+	github.com/golang/protobuf v1.5.4
+	github.com/jhump/protoreflect v1.16.0
 	github.com/nats-io/nats.go v1.35.0
 	github.com/openGemini/opengemini-client-go v0.5.1
 	github.com/rabbitmq/amqp091-go v1.10.1-0.20240821123418-dc67c21576c2
 	github.com/redis/go-redis/v9 v9.5.2
-	github.com/rulego/rulego v0.23.1-0.20240906092025-fda1952d9f9e
+	github.com/rulego/rulego v0.27.1-0.20250104080653-1dec14655d35
 	github.com/vadv/gopher-lua-libs v0.5.0
 	github.com/yuin/gopher-lua v1.1.1
 	go.mongodb.org/mongo-driver v1.16.1
+	go.opentelemetry.io/otel v1.33.0
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v1.33.0
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp v1.33.0
+	go.opentelemetry.io/otel/metric v1.33.0
+	go.opentelemetry.io/otel/sdk/metric v1.33.0
+	google.golang.org/grpc v1.68.1
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/VividCortex/ewma v1.1.1 // indirect
 	github.com/alessio/shellescape v1.4.1 // indirect
 	github.com/aws/aws-sdk-go v1.34.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bufbuild/protocompile v0.10.0 // indirect
 	github.com/cbroglie/mustache v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cheggaaa/pb/v3 v3.0.5 // indirect
-	github.com/cncf/xds/go v0.0.0-20231109132714-523115ebc101 // indirect
+	github.com/cncf/xds/go v0.0.0-20240905190251-b4127c9b8d78 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dlclark/regexp2 v1.7.0 // indirect
@@ -34,17 +44,20 @@ require (
 	github.com/eapache/go-resiliency v1.6.0 // indirect
 	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
 	github.com/eapache/queue v1.1.0 // indirect
-	github.com/eclipse/paho.mqtt.golang v1.4.2 // indirect
-	github.com/envoyproxy/go-control-plane v0.11.1 // indirect
-	github.com/envoyproxy/protoc-gen-validate v1.0.2 // indirect
+	github.com/eclipse/paho.mqtt.golang v1.4.3 // indirect
+	github.com/envoyproxy/go-control-plane v0.13.0 // indirect
+	github.com/envoyproxy/protoc-gen-validate v1.1.0 // indirect
 	github.com/fatih/color v1.7.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
-	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/gofrs/uuid/v5 v5.0.0 // indirect
-	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/pprof v0.0.0-20230207041349-798e818bf904 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.24.0 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
@@ -53,7 +66,6 @@ require (
 	github.com/jcmturner/gofork v1.7.6 // indirect
 	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
-	github.com/jhump/protoreflect v1.16.0 // indirect
 	github.com/jmespath/go-jmespath v0.3.0 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/klauspost/compress v1.17.8 // indirect
@@ -67,28 +79,33 @@ require (
 	github.com/nats-io/nkeys v0.4.7 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.19.1 // indirect
-	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/client_model v0.6.0 // indirect
 	github.com/prometheus/common v0.48.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
-	github.com/robfig/cron/v3 v3.0.0 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	github.com/yuin/gluamapper v0.0.0-20150323120927-d836955830e7 // indirect
-	golang.org/x/crypto v0.22.0 // indirect
-	golang.org/x/net v0.24.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/sys v0.19.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17 // indirect
-	google.golang.org/grpc v1.61.0 // indirect
-	google.golang.org/protobuf v1.33.1-0.20240408130810-98873a205002 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.33.0 // indirect
+	go.opentelemetry.io/otel/trace v1.33.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.4.0 // indirect
+	golang.org/x/crypto v0.30.0 // indirect
+	golang.org/x/net v0.32.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20241209162323-e6fa225c2576 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241209162323-e6fa225c2576 // indirect
+	google.golang.org/protobuf v1.35.2 // indirect
 	gopkg.in/xmlpath.v2 v2.0.0-20150820204837-860cbeca3ebc // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
